ccb9a7408ded52e8d2958ba66769569095136d61
guru2711 <saiguruprakash@gmail.com>
2021-12-07 12:37:16 +0530

    "app completed"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
+import "./App.css";
+import io from "socket.io-client";
+import { useState } from "react";
+import Chat from "./chat";
 
-import './App.css';
+var socket = io.connect("http://localhost:3001");
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [chat, setShowChat] = useState(false);
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+      setShowChat(true);
+
+      // console.log(username + " " + room);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>App js</h1>
-      </header>
+      {!chat ? (
+        <div className="joinChatContainer">
+          <h3>Join a Chat</h3>
+          <input
+            type="text"
+            placeholder="Name..."
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Room ID..."
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Join A Room</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
+      )}
     </div>
   );
 }
---
+++ src/chat.js
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from "react";
+import ScrollToBottom from "react-scroll-to-bottom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export default function Chat({ socket, username, room }) {
+  const [currentmsg, setCurrentmsg] = useState("");
+  const [msglist, setMsglist] = useState([]);
+
+  const sendMsg = async () => {
+    if (currentmsg !== "") {
+      const messageData = {
+        room: room,
+        author: username,
+        message: currentmsg,
+        time: formatAMPM(new Date()),
+      };
+
+      await socket.emit("send_message", messageData);
+      setMsglist((list) => [...list, messageData]);
+      setCurrentmsg("");
+    }
+  };
+
+  useEffect(() => {
+    socket.on("receive_msg", (data) => {
+      setMsglist((list) => [...list, data]);
+      console.log(data);
+    });
+  }, [socket]);
+
+  useEffect(() => {
+    toast(`welcome, You are in  Room: ${room}`);
+  }, []);
+
+  return (
+    <div className="chat-window">
+      <ToastContainer />
+      <div className="chat-header">
+        <p>Room : {room}</p>
+      </div>
+      <div className="chat-body">
+        <ScrollToBottom className="message-container">
+          {msglist.map((e) => {
+            return (
+              <div
+                className="message"
+                id={username === e.author ? "you" : "other"}
+              >
+                <div>
+                  <div className="message-content">
+                    <p>{e.message}</p>
+                  </div>
+                  <div className="message-meta">
+                    <p id="time">{e.time}</p>
+                    <p id="author">{e.author}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </ScrollToBottom>
+      </div>
+      <div className="chat-footer">
+        <input
+          type="text"
+          placeholder="Hey!..."
+          value={currentmsg}
+          onChange={(event) => {
+            setCurrentmsg(event.target.value);
+          }}
+          onKeyPress={(e) => {
+            e.key === "Enter" && sendMsg();
+          }}
+        />
+        <button onClick={sendMsg}>send</button>
+      </div>
+    </div>
+  );
+}
+
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? "pm" : "am";
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  var strTime = hours + ":" + minutes + " " + ampm;
+  return strTime;
+}
+
+console.log(formatAMPM(new Date()));
