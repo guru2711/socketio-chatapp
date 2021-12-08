5139d1c071d1caf57c4626790f61f40153ee1ee4
guru2711 <saiguruprakash@gmail.com>
2021-12-08 21:24:23 +0530

    "done"

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./chat";
 
-var socket = io.connect("http://localhost:3001");
+var socket = io.connect("https://socketio-chata.herokuapp.com");
 
 function App() {
   const [username, setUsername] = useState("");
